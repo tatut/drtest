3ea11d7c710cb3b7522c0534690215f29c018403
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-06-01 10:22:32 +0300

    Add description and url

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject webjure/drtest "20190601"
+  :description "Declarative Reagent UI testing library"
+  :url "https://github.com/tatut/drtest"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.520"]
                  [reagent "0.8.1"]]
