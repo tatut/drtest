3848e7f9918f735b19be7204652e7197cbf14519
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-06-03 11:16:52 +0300

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject webjure/drtest "20190603-SNAPSHOT"
+(defproject webjure/drtest "20190603"
   :description "Declarative Reagent UI testing library"
   :url "https://github.com/tatut/drtest"
   :dependencies [[org.clojure/clojure "1.10.0"]
